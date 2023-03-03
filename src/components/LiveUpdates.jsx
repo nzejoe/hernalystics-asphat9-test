5371afaf6cddb612b04a5382ce64eacf44d2f918
Jonathan Nzete <jonathannzete@gmail.com>
2023-03-03 11:22:27 +0100

    house of rep stats live updates

--- src/components/LiveUpdates.jsx
+++ src/components/LiveUpdates.jsx
@@ -65,6 +65,39 @@ const LiveUpdates = ({ electEvent, handleSelectEvent }) => {
             color: "#D62B3C",
         },
     ];
+    const housePolls = [
+        {
+            id: "1",
+            partyName1: "All Progressive",
+            partyName2: "Congress",
+            partyCode: "APC",
+            avatar: "images/apc-logo.svg",
+            seats: "54",
+            percentage: "51.3%",
+            color: "#64CCFF",
+        },
+        {
+            id: "2",
+            partyName1: "The",
+            partyName2: "Labor Party",
+            partyCode: "LP",
+            avatar: "images/lp-logo.svg",
+            seats: "12",
+            percentage: "16.3%",
+            color: "#0AA83F",
+        },
+        {
+            id: "3",
+            partyName1: "People",
+            partyName2: "Democratic Party",
+            partyCode: "PDP",
+            avatar: "images/pdp-logo.svg",
+            seats: "36",
+            percentage: "46%",
+            color: "#D62B3C",
+        },
+    ];
+
     return (
         <div>
             <div className="flex justify-between">
@@ -124,52 +157,107 @@ const LiveUpdates = ({ electEvent, handleSelectEvent }) => {
 
             {/* SENATE POLLS */}
             <div className="mt-[130px]">
-                <h5 className="text-2xl font-medium mb-6">SENATE</h5>
-                <div className="flex">
-                    <div className="flex-1 flex">
-                        {senPolls.map((poll) => (
-                            <div key={poll.id} style={{ width: `${poll.percentage}` }} className="relative">
-                                <div className={` h-[16px]`} style={{ backgroundColor: poll.color }}></div>
-                                <div
-                                    className={`absolute top-[20px] ${
-                                        poll.partyCode === "LP"
-                                            ? "left-1/2 transform -translate-x-1/2"
-                                            : poll.partyCode === "PDP"
-                                            ? "right-0 "
-                                            : ""
-                                    }`}
-                                >
-                                    <div className={`relative`}>
-                                        <img
-                                            src="images/point-up-icon.svg"
-                                            alt=""
-                                            className={`absolute top-0 left-1/2`}
-                                        />
-                                    </div>
-                                    <div className={`flex bg-white p-2 gap-2 min-w-[180px] mt-2 `}>
-                                        <div>
-                                            <img src={`${poll.avatar}`} alt="" />
+                <div className="flex gap-[120px]">
+                    <div className="flex-1">
+                        <h5 className="text-2xl font-medium mb-6">SENATE</h5>
+                        <div className="flex">
+                            {senPolls.map((poll) => (
+                                <div key={poll.id} style={{ width: `${poll.percentage}` }} className="relative">
+                                    <div className={` h-[16px]`} style={{ backgroundColor: poll.color }}></div>
+                                    <div
+                                        className={`absolute top-[20px] ${
+                                            poll.partyCode === "LP"
+                                                ? "left-1/2 transform -translate-x-1/2"
+                                                : poll.partyCode === "PDP"
+                                                ? "right-0 "
+                                                : ""
+                                        }`}
+                                    >
+                                        <div className={`relative`}>
+                                            <img
+                                                src="images/point-up-icon.svg"
+                                                alt=""
+                                                className={`absolute top-0 left-1/2`}
+                                            />
                                         </div>
-                                        <div>
-                                            <p style={{ color: poll.color }} className="text-sm font-medium">
-                                                <span>
-                                                    {poll.partyName1} <br />
-                                                    {poll.partyName2}
-                                                </span>
-                                            </p>
-                                            <p className="text-[#585858] text-sm">
-                                                {poll.seats} Seats <span>{poll.percentage}</span>
-                                            </p>
+                                        <div className={`flex bg-white p-2 gap-2 min-w-[180px] mt-2 `}>
+                                            <div>
+                                                <img src={`${poll.avatar}`} alt="" />
+                                            </div>
+                                            <div>
+                                                <p style={{ color: poll.color }} className="text-sm font-medium">
+                                                    <span>
+                                                        {poll.partyName1} <br />
+                                                        {poll.partyName2}
+                                                    </span>
+                                                </p>
+                                                <p className="text-[#585858] text-sm">
+                                                    {poll.seats} Seats <span>{poll.percentage}</span>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                    <div className="flex-1"></div>
+                    <div className="flex-1">
+                        <h5 className="text-2xl font-medium mb-6">HOUSE</h5>
+                        <div className="flex">
+                            {senPolls.map((poll) => (
+                                <div key={poll.id} style={{ width: `${poll.percentage}` }} className="relative">
+                                    <div className={` h-[16px]`} style={{ backgroundColor: poll.color }}></div>
+                                    <div
+                                        className={`absolute top-[20px] ${
+                                            poll.partyCode === "LP"
+                                                ? "left-1/2 transform -translate-x-1/2"
+                                                : poll.partyCode === "PDP"
+                                                ? "right-0 "
+                                                : ""
+                                        }`}
+                                    >
+                                        <div className={`relative`}>
+                                            <img
+                                                src="images/point-up-icon.svg"
+                                                alt=""
+                                                className={`absolute top-0 left-1/2`}
+                                            />
+                                        </div>
+                                        <div className={`flex bg-white p-2 gap-2 min-w-[180px] mt-2 `}>
+                                            <div>
+                                                <img src={`${poll.avatar}`} alt="" />
+                                            </div>
+                                            <div>
+                                                <p style={{ color: poll.color }} className="text-sm font-medium">
+                                                    <span>
+                                                        {poll.partyName1} <br />
+                                                        {poll.partyName2}
+                                                    </span>
+                                                </p>
+                                                <p className="text-[#585858] text-sm">
+                                                    {poll.seats} Seats <span>{poll.percentage}</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="mt-[130px]">
+                <div className="flex justify-between mb-[54px]">
+                    <h5 className="text-2xl font-medium">Presidential Race</h5>
+                    <div className="flex items-center gap-2">
+                        <h5>Filter By:</h5>
+                        <select name="" id="" className="dark-box px-2 py-1">
+                            <option value="">Presidential</option>
+                            <option value="">Senate</option>
+                            <option value="">House of Representative</option>
+                        </select>
+                    </div>
+                </div>
                 <ElectionMap />
             </div>
         </div>
