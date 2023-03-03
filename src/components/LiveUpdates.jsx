1f2d9a0f2a94506a7ca1eec40a95a8a2e4b330b4
Jonathan Nzete <jonathannzete@gmail.com>
2023-03-03 09:38:54 +0100

    live updates presidential section

--- src/components/LiveUpdates.jsx
+++ src/components/LiveUpdates.jsx
@@ -3,6 +3,35 @@ import HomeSelect from "./HomeSelect";
 import ElectionMap from "./ElectionMap";
 
 const LiveUpdates = ({ electEvent, handleSelectEvent }) => {
+    const presPolls = [
+        {
+            id: "1",
+            partyName: "APC",
+            contestant: "Tinubu",
+            avatar: "images/tinubu-avatar.svg",
+            number: "81,283,786",
+            percentage: "51.3%",
+            color: "#64CCFF",
+        },
+        {
+            id: "2",
+            partyName: "LP",
+            contestant: "Peter",
+            avatar: "images/obi-avatar.svg",
+            number: "60,283,786",
+            percentage: "42%",
+            color: "#0AA83F",
+        },
+        {
+            id: "3",
+            partyName: "PDP",
+            contestant: "Atiku",
+            avatar: "images/atiku-avatar.svg",
+            number: "283,786",
+            percentage: "7.3%",
+            color: "#D62B3C",
+        },
+    ];
     return (
         <div>
             <div className="flex justify-between">
@@ -24,8 +53,43 @@ const LiveUpdates = ({ electEvent, handleSelectEvent }) => {
                 <HomeSelect electEvent={electEvent} handleSelectEvent={handleSelectEvent} />
             </div>
             <h5 className="text-2xl font-medium mb-6">PRESIDENT</h5>
-            <div className=""></div>
-            <ElectionMap />
+
+            <div className="flex">
+                {presPolls.map((poll) => (
+                    <div key={poll.id} style={{ width: `${poll.percentage}` }} className="relative">
+                        <div className={` h-[16px]`} style={{ backgroundColor: poll.color }}></div>
+                        <div className={`absolute top-[20px] ${poll.partyName === "PDP" ? "right-2" : "left-2"}`}>
+                            <div className={`relative`}>
+                                <img
+                                    src="images/point-up-icon.svg"
+                                    alt=""
+                                    className={`absolute top-0 ${poll.partyName === "PDP" ? "right-3" : "left-4"}`}
+                                />
+                            </div>
+                            <div
+                                className={`flex bg-white p-2 gap-2 min-w-[200px] mt-2 ${
+                                    poll.partyName === "PDP" ? "right-2" : "left-2"
+                                }`}
+                            >
+                                <div>
+                                    <img src={`${poll.avatar}`} alt="" />
+                                </div>
+                                <div>
+                                    <p style={{ color: poll.color }} className="text-sm font-medium">
+                                        <span>{poll.contestant}</span>,<span>{poll.partyName}</span>
+                                    </p>
+                                    <p className="text-[#585858] text-sm">
+                                        {poll.number} votes <span>{poll.percentage}</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="mt-[130px]">
+                <ElectionMap />
+            </div>
         </div>
     );
 };
