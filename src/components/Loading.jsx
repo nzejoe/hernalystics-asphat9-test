a00a317021f179b5eb68830b5b6612be6fcf55bb
Jonathan Nzete <jonathannzete@gmail.com>
2023-03-10 16:29:06 +0100

    loading component added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+    return (
+        <div>
+            <button className="btn border-none bg-transparent loading text-white"></button>
+        </div>
+    );
+};
+export default Loading;
